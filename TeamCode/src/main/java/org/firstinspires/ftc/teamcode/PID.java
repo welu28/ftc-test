da442d70971e5396c83317686856591749346987
welu28 <27luw@millburn.org>
2024-07-15 12:11:09 -0400

    Update PID.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -1,3 +1,14 @@
-public class PID {
-    
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class PID extends LinearOpMode {
+    public static double
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
