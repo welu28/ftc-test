37fa25a0771420b688dee71fe945d3c1edaec1b3
welu28 <27luw@millburn.org>
2024-07-20 14:58:44 -0400

    d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorEncoder.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp
+@Autonomous
 public class MotorEncoder extends LinearOpMode {
     private Drivetrain drivetrain;
     private DcMotor fL;
