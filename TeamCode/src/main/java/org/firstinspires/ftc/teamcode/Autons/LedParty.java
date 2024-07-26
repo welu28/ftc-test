e0deb81478c7799065fa5462cd618e657467bbff
welu28 <151058854+welu28@users.noreply.github.com>
2024-07-26 14:39:31 -0400

    h

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autons/LedParty.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Autons;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Subsystems.LED;
+
+@Autonomous
+public class LedParty extends LinearOpMode {
+    private LED led;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        led = new LED();
+        led.init(hardwareMap);
+        led.startParty();
+        while(opModeIsActive()) {
+            led.update();
+        }
+        led.endParty();
+    }
+}
