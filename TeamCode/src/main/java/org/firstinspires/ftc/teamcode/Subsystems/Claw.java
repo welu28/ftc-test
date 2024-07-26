792e06ea19a5e514be223240e718c1fd0d4dcf9a
welu28 <151058854+welu28@users.noreply.github.com>
2024-07-26 14:44:35 -0400

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Claw.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
-    public Servo claw
+    public Servo claw;
     public Servo bottom;
     public Servo top;
 
